f6b726f70b2f9e519f4708a895d2f9a0a92922e1
Ayush Priya <ayushpriya10@gmail.com>
2019-10-28 00:47:51 +0530

    Renamed functions.

--- pkg_uninstallation.py
+++ pkg_uninstallation.py
@@ -35,7 +35,7 @@ def perform_remove_module(conn, packages_to_uninstall, db):
 
 
 
-def sell(packages_to_uninstall, db, engine):
+def uninstall_packages(packages_to_uninstall, db, engine):
     conn = open_database(engine)
     perform_remove_module(conn, packages_to_uninstall, db)
     update_requirements_file()
