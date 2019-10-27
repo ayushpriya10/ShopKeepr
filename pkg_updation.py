92f74a5960223da31c61134c0816b2e5645c2057
Ayush Priya <ayushpriya10@gmail.com>
2019-10-27 23:35:08 +0530

    Refactored pkg_updation.py

--- pkg_updation.py
+++ pkg_updation.py
@@ -4,11 +4,14 @@ from functions import check_if_exists, install, get_version
 def restock(packages_to_update, db, conn):
     for package in packages_to_update:
         pid = check_if_exists(package)
+        
         if pid is not None:
             install(package)
             update_package_query = db.update().values(version=get_version(package)).where(db.c.name == package)
             conn.execute(update_package_query)
+        
             return True
+        
         else:
             return False
 
