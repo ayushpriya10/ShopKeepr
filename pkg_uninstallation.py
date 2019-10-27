74277c635584f532c1649984f7c17d44a28d1626
Ayush Priya <ayushpriya10@gmail.com>
2019-10-27 23:34:23 +0530

    Refactored pkg_uninstallation.py

--- pkg_uninstallation.py
+++ pkg_uninstallation.py
@@ -5,11 +5,14 @@ def delete_dependencies(conn, parent_pid, db):
     select_dependencies = db.select().where(db.c.parent_id == parent_pid)
     result = conn.execute(select_dependencies)
     flag = True
+    
     for _row in result:
         common_dependencies = db.select().where(db.c.name == _row[1])
+        
         for _com_dep in common_dependencies:
             if _com_dep[3] != parent_pid:
                 flag = False
+        
         if flag:
             uninstall(_row[1])
 
@@ -20,11 +23,13 @@ def delete_dependencies(conn, parent_pid, db):
 def perform_remove_module(conn, packages_to_uninstall, db):
     for package in packages_to_uninstall:
         parent_pid = check_if_exists(conn, package, db)
+        
         if parent_pid is not None:
             delete_dependencies(conn, parent_pid, db)
             delete_package(conn, package, parent_pid, db)
         else:
             print("Package has not been installed")
+        
             return False
 
 
@@ -32,7 +37,6 @@ def perform_remove_module(conn, packages_to_uninstall, db):
 
 def sell(packages_to_uninstall, db, engine):
     conn = open_database(engine)
-
     perform_remove_module(conn, packages_to_uninstall, db)
 
 
